ced6adff6ff4f0520e7a92811cde72889f10ee97
Wallace Vinicius <wallace@lavid.ufpb.br>
2019-03-09 06:16:37 -0300

    Site

---
+++ static/js/global.js
@@ -0,0 +1,53 @@
+//$("body").css("filter", "invert(100%)").css("background", "black");
+//$("img.block").css("filter", "invert(100%)")
+
+
+function titleAnimation() {
+	$('header .container').addClass("animated bounceInDown");
+}
+
+function menu(){
+	$("#sidebar").hide();
+	$("#icon-menu").click(function(){
+		$("#sidebar").show().animate({
+			right: -22
+		}), 100;
+	});
+}
+
+function masonry() {
+	$("#works .items .block").css("display", "none");
+	$("#works .items .block").slice(0, 7).css("display", "inline-block");
+	$("#loadMore").on('click', function (e) {
+	    e.preventDefault();
+	    $("#works .items .block:hidden").slice(0, 4).slideDown().css("display", "inline-block");
+	    if ($("#works .items .block:hidden").length == 0) {
+	        $("#loadMore").css("opacity", 0.5);
+	    }
+	    $('html,body').animate({
+	        scrollTop: $(this).offset().top
+	    }, 1500);
+	});	
+}
+
+function buttonTop(){
+	var btn = $('#toTop');
+
+	$(window).scroll(function() {
+		if ($(window).scrollTop() > 300) {
+			btn.addClass('show');
+		} else {
+			btn.removeClass('show');
+		}
+	});
+
+	btn.on('click', function(e) {
+		e.preventDefault();
+		$('html, body').animate({scrollTop:0}, '300');
+	});
+}
+
+titleAnimation();
+menu();
+
+
